de8f1e5959866b3b6b10b8fa0022a50c92e31189
liuzheng <liuzheng712@gmail.com>
2019-10-02 10:25:55 +0800

    feat: update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+interface Link {
+  image: string;
+  url: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'quick-page';
+  list: Array<Link>;
+
+  constructor() {
+    this.list = [{
+      image: 'assets/imgs/taobao.svg',
+      url: 'https://www.taobao.com/',
+      name: '淘宝'
+    }, {
+      image: 'assets/imgs/alibaba.svg',
+      url: 'https://www.alibaba.com/',
+      name: '阿里巴巴'
+    }, {
+      image: 'assets/imgs/baidu.svg',
+      url: 'https://www.baidu.com/',
+      name: '百度'
+    }, {
+      image: 'assets/imgs/icbc.png',
+      url: 'https://www.icbc.com.cn/',
+      name: '工商银行'
+    }, {
+      image: 'assets/imgs/youzhen.jpg',
+      url: 'http://www.psbc.com/',
+      name: '邮政银行'
+    }];
+  }
+
 }
