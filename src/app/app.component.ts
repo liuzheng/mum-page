28a8918a51bac88460083f72cbe8d674057ba130
liuzheng <liuzheng712@gmail.com>
2019-10-02 10:52:35 +0800

    feat: udpate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,27 +17,27 @@ export class AppComponent {
 
   constructor() {
     this.list = [{
-      image: 'assets/imgs/taobao.svg',
+      image: '/assets/imgs/taobao.svg',
       url: 'https://www.taobao.com/',
       name: '淘宝'
     }, {
-      image: 'assets/imgs/alibaba.svg',
+      image: '/assets/imgs/alibaba.svg',
       url: 'https://www.alibaba.com/',
       name: '阿里巴巴'
     }, {
-      image: 'assets/imgs/baidu.svg',
+      image: '/assets/imgs/baidu.svg',
       url: 'https://www.baidu.com/',
       name: '百度'
     }, {
-      image: 'assets/imgs/icbc.png',
+      image: '/assets/imgs/icbc.png',
       url: 'https://www.icbc.com.cn/',
       name: '工商银行'
     }, {
-      image: 'assets/imgs/youzhen.jpg',
+      image: '/assets/imgs/youzhen.jpg',
       url: 'http://www.psbc.com/',
       name: '邮政银行'
     }, {
-      image: 'assets/imgs/jd.png',
+      image: '/assets/imgs/jd.png',
       url: 'https://www.jd.com/',
       name: '京东'
     }];
